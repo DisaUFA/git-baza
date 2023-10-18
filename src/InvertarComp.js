36207559854bf85f38b3b5a0e1a0f44cb26bbf05
DisaRCDO <denisyxxx@mail.ru>
2023-10-18 12:20:10 +0500

    18.10.23

---
+++ src/InvertarComp.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+export default function Invertar() {
+  return (
+    
+
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <h2>Инвентарные номера</h2>
+    <div class="table-responsive small">
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th scope="col">№</th>
+            <th scope="col">Оборудование</th>
+            <th scope="col">Цена</th>
+            <th scope="col">Инвентарный номер</th>
+            <th scope="col">N: Гос.Контракта</th>
+            <th scope="col">Филиал</th>
+            <th scope="col">Место нахождения</th>
+          </tr>
+        </thead>
+        <tbody>
+
+
+
+
+          <tr>
+            <td>1</td>
+            <td>Графический планшет Wecom Bamboo Pen CTL-470K-RUPL</td>
+            <td>3100.00</td>
+            <td>ЯЯ0000012347</td>
+            <td>№251 - от2010-11-02</td>
+            <td>Уфа</td>
+            <td>Договор N: 1678 - Ученик</td>
+          </tr>
+          
+        </tbody>
+      </table>
+    </div>
+    </main>
+
+  
+  )
+}
